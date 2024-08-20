ce1217f29033ad992ae02dc3d2a83c017ff46806
Svitlana <160290680+Svitlana-2020@users.noreply.github.com>
2024-08-20 13:55:18 +0100

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,10 @@
 // import viteLogo from '/vite.svg'
 import '../App.css'
 import ProfileCard from './ProfileCard'
+import userData from '../userdata.json'
 
 
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308
-  }
-};
-
-
-
-function App() {
-  // const [count, setCount] = useState(0)
-
+export default function App () {
   return (
     <>
 <ProfileCard user={userData} />
@@ -29,4 +14,4 @@ function App() {
   )
 }
 
-export default App
+
